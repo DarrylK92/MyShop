6aae63749f88a2e7bbc4ca31d8f131284e4bcb9b
Darryl Karney <karneyd@outlook.com>
2020-11-30 19:37:24 -0600

    Add Dependency Injection

---
+++ MyShop.Core/Contracts/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        void Commit();
+        void Insert(T t);
+        void Update(T t);
+        T Find(string Id);
+        IQueryable<T> Collection();
+        void Delete(string Id);
+    }
+}
