0cb6307e289e738ecf422a6e1566ff6ecff4f46c
Darryl Karney <karneyd@outlook.com>
2020-12-01 20:47:23 -0600

    Implement SQL Repository

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.SQL;
 
 namespace MyShop.WebUI.Controllers
 {
