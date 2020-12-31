dd98e839b5de430b7745a6b2bce51d3699e19d27
Darryl Karney <karneyd@outlook.com>
2020-12-30 20:10:45 -0600

    Add order creation

---
+++ MyShop.Services/OrderService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.Services
+{
+    public class OrderService : IOrderService
+    {
+        IRepository<Order> orderContext;
+        public OrderService(IRepository<Order> orderContext)
+        {
+            this.orderContext = orderContext;
+        }
+
+        public void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems)
+        {
+            foreach (var item in basketItems)
+            {
+                baseOrder.OrderItems.Add(new OrderItem()
+                {
+                    ProductId = item.id,
+                    Image = item.image,
+                    Price = item.price,
+                    ProductName = item.productName,
+                    Quantity = item.quantity
+                });
+            }
+
+            orderContext.Insert(baseOrder);
+            orderContext.Commit();
+        }
+    }
+}
--- MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop.WebUI/Controllers/BasketController.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 
 namespace MyShop.WebUI.Controllers
 {
     public class BasketController : Controller
     {
         private IBasketService basketService;
+        private IOrderService orderService;
 
-        public BasketController(IBasketService BasketService)
+        public BasketController(IBasketService BasketService, IOrderService orderService)
         {
             this.basketService = BasketService;
+            this.orderService = orderService;
         }
         // GET: Basket
         public ActionResult Index()
@@ -41,6 +44,32 @@ namespace MyShop.WebUI.Controllers
             var basketSummary = basketService.GetBasketSummary(this.HttpContext);
 
             return PartialView(basketSummary);
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Checkout(Order order)
+        {
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
+            order.OrderStatus = "Order Created";
+
+            //process payment
+
+            order.OrderStatus = "Payment Processed";
+            orderService.CreateOrder(order, basketItems);
+            basketService.ClearBasket(this.HttpContext);
+
+            return RedirectToAction("ThankYou", new {OrderId = order.Id});
+        }
+
+        public ActionResult Thankyou(string OrderId)
+        {
+            ViewBag.OrderId = OrderId;
+            return View();
         }
     }
 }
